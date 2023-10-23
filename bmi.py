7d1c929d4de1dabbfb03eb4882ac08b94985a86e
Thaqif03 <AHMADALTHAQIF03.22@ichat.sp.edu.sg>
2023-10-23 10:28:18 +0800

    lab 2 & bmi V1.0

--- bmi.py
+++ bmi.py
@@ -3,10 +3,11 @@ def calculate_bmi(height, weight):
     print("Weight = " + str(weight))
 
     # Add code here to calculate BMI
+
     bmi =(weight/(height*height))
 
     # Add code here to display calculate BMI
+    
     print(bmi)
-
 
 calculate_bmi(weight=57, height=1.73)
